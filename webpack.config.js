bd05c50a944d883fab150a544342c1ab83103d8b
Marcin Owczarzak <marcin@adchitects.co>
2021-03-16 09:51:21 +0100

    Webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -11,12 +11,12 @@ const userConfig = {
 	},
 	distDirectory: `../${path.basename(__dirname)}-dist/`,
 	distFiles: [
-		'*.html', 
-		'style.css',  
-		'src/img/', 
-		'src/fonts/', 
-		'src/css/style.min.css', 
-		'src/js/main.min.js',
+		'*.twig', 
+		'web/src/img/', 
+		'web/src/fonts/', 
+		'web/src/uploads/', 
+		'web/src/css/style.min.css', 
+		'web/src/js/main.min.js',
 	],
 };
 const webpackConfig = {
