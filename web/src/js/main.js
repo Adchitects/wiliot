fa6c8dacb5c8cfd69f97c6a2db922c0ab269ff3e
Ad_Workstation_14 <rogalski.kry@gmail.com>
2021-03-23 11:10:34 +0100

    company page sections and assets

--- web/src/js/main.js
+++ web/src/js/main.js
@@ -1,5 +1,8 @@
 // SCRIPTS
 import './scripts/partnersFilters';
+import './scripts/accordion';
+import './scripts/bubbles';
+
 // OLD
 import './scripts/sliders';
 import './scripts/doge';
---
+++ web/src/js/scripts/accordion.js
@@ -0,0 +1,32 @@
+const accordion = document.querySelector('.accordion');
+
+if (accordion) {
+	const accordionItems = document.querySelectorAll('.js-accordion-toggle-item');
+	const accordionList = document.querySelector('.accordion__list');
+
+
+	accordionItems.forEach(item => {
+		item.addEventListener('click', () => {
+			if (item.classList.contains('is-active')) {
+				item.classList.remove('is-active');
+			} else {
+				accordionItems.forEach(elem => {
+					elem.classList.remove('is-active');
+				});
+				item.classList.toggle('is-active');
+			}
+		});
+	});
+
+	document.addEventListener('click', (e) => {
+		let targetElement = e.target;
+
+		let isClickInside = accordionList.contains(targetElement);
+
+		if (!isClickInside) {
+			accordionItems.forEach(elem => {
+				elem.classList.remove('is-active');
+			});
+		}
+	});
+}
