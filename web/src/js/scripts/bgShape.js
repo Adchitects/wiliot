062106ba60757bff59be5a80b21cc222a2b4a41d
Marcin Owczarzak <marcin@adchitects.co>
2021-04-21 16:30:02 +0200

    Demo

--- web/src/js/scripts/bgShape.js
+++ web/src/js/scripts/bgShape.js
@@ -30,6 +30,7 @@ bgShapeHldAll.forEach(hld => {
 			const shape2 = bgShape.querySelector('.js-s2');
 			const shape3 = bgShape.querySelector('.js-s3');
 			tl.to(shape1, {
+				y: 100,
 				duration: 6,
 				opacity: 1,
 				rotation: 5,
@@ -45,16 +46,46 @@ bgShapeHldAll.forEach(hld => {
 				rotation: 30,
 			}, 4);
 			tl.to(shape3, {
-				duration: 2,
+				duration: 4,
+				x: 200,
 				opacity: 1,
 				rotation: -15,
 			}, 4);
+		}
+		if (bgShapeVersion === 'center-right') {
+			const shape1 = bgShape.querySelector('.js-s1');
+			tl.to(shape1, {
+				duration: 15,
+				y: 100,
+				scaleX: 2,
+				rotationX: -45,
+				transformOrigin: 'center right',
+			});
+			tl.to(shape1, {
+				duration: 7,
+				scaleX: 0.5,
+			});
+		}
+		if (bgShapeVersion === 'bottom-right') {
+			const shape1 = bgShape.querySelector('.js-s1');
+			const shape2 = bgShape.querySelector('.js-s2');
+			tl.to(shape1, {
+				duration: 10,
+				opacity: 0,
+				scale: 4,
+			});
+			tl.to(shape2, {
+				duration: 6,
+				opacity: 1,
+				rotation: -15,
+			}, 1);
 		}
 		if (bgShapeVersion === 'bottom-left') {
 			const shape1 = bgShape.querySelector('.js-s1');
 			const shape2 = bgShape.querySelector('.js-s2');
 			tl.to(shape1, {
 				duration: 4,
+				x: -100,
 				opacity: 0,
 				scale: 1.2,
 			});
@@ -63,6 +94,22 @@ bgShapeHldAll.forEach(hld => {
 				opacity: 1,
 				rotation: 10,
 			}, 1);
+		}
+		if (bgShapeVersion === 'center-left') {
+			const shape1 = bgShape.querySelector('.js-s1');
+			tl.to(shape1, {
+				duration: 15,
+				y: -100,
+				scaleX: 2,
+				rotationX: 45,
+				transformOrigin: 'center left',
+			});
+			tl.to(shape1, {
+				duration: 15,
+				y: 300,
+				scaleX: 3,
+				scaleY: 2,
+			});
 		}
 	});
 });
