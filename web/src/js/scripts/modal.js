cb70c67cd47a3bc4b88dda470b18ce94cc238f7b
Marcin Owczarzak <marcin@adchitects.co>
2021-06-09 16:02:27 +0200

    Change requests

--- web/src/js/scripts/modal.js
+++ web/src/js/scripts/modal.js
@@ -3,15 +3,22 @@ modalHldAll.forEach(modalHld => {
 	const modal = modalHld.querySelector('.js-modal-modal');
 	const openModal = modalHld.querySelector('.js-modal-btn');
 	const closeModalAll = modalHld.querySelectorAll('.js-modal-close');
+	const iframe = modalHld.querySelector('.js-modal-iframe');
 	if (modal && openModal) {
 		openModal.addEventListener('click', () => {
 			modal.classList.add('is-active');
 			document.body.style.overflow = 'hidden';
+			if (iframe) {
+				iframe.src = iframe.dataset.src;
+			}
 		});
 		closeModalAll.forEach(close => {
 			close.addEventListener('click', () => {
 				modal.classList.remove('is-active');
 				document.body.style.overflow = '';
+				if (iframe) {
+					iframe.src = '';
+				}
 			});
 		});
 	}
