58b131815e81fadeee09f11f07a265a75ab0cce3
Marcin Owczarzak <marcin@adchitects.co>
2021-07-14 15:15:51 +0200

    Backup

--- web/src/js/scripts/fusionAuth.js
+++ web/src/js/scripts/fusionAuth.js
@@ -9,31 +9,38 @@ const key = {
 	'n': 'pH-2BjhKO4MiPf5VXRVTc_vnBO00CqI0NOq-g3Y5lpIv3a-IYsnswnfhm5yQha7OGlTRh5Nfpys2NZybuzY5MOe5I3EIJEZzHw5racQPsbJ6FmVdiF8O0Pikxgxlq5gihiWjdWQ9dmaqyARwdgbOfpB3jvIFYR7Ca02ykqSvhtEch6sYwJqlZWzmc10v3bVrKLieN6YqDOYdvzG__5VVqpN4Tj-I0aNq5LPfCF0sSiHBS76tl941U0v5Dy1XCZmaINlqehn1MGmZBr9pj40-BX90boly8O67B9rkhYGwicVqUkuFarTZBKA1xEbQuus_KpdgyoIFl5rYM4jGOgKgMw',
 	'use': 'sig',
 };
+const pem = jwkToPem(key);
+const options = { algorithms: ['RS256'] };
+const isLoginLinkText = document.querySelector('.menu-desktop__list-item.is-login .text');
 const login = () => {
 	const host = 'https://api.wiliot.com';
 	const eventPath = ''; // What is the value? “/”
 	const path = `${eventPath}/v1/auth/token`;
 	const clientId = '21ffd444-76e2-4c3f-9317-38c5fddb169d'; // What is the value? Is it 21ffd444-76e2-4c3f-9317-38c5fddb169d ?
-	const redirectUri = `${window.location.origin}/login`;
+	// const redirectUri = `${window.location.origin}/login`;
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	const code = urlParams.get('code');
-	const url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=${redirectUri}&grant_type=authorization_code`;
+	// const url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=${redirectUri}&grant_type=authorization_code`;
+	const url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=https://www.wiliot.com/login&grant_type=authorization_code`;
 	console.log(code);
 	if (code) {
 		axios.post(url, null, {}).then((response) => {
 			localStorage.setItem('access_token', response.data.access_token);
 			localStorage.setItem('refresh_token', response.data.refresh_token);
 			localStorage.setItem('expires_in', response.data.expires_in);
-			const pem = jwkToPem(key);
-			const options = { algorithms: ['RS256'] };
+			console.log('logged in');
 			const decoded = jwt.verify(response.data.access_token, pem, options);
 			const user = {
 				email: decoded.email,
 				username: decoded.preferred_username,
 			};
-			console.log('logged in');
 			console.log(user);
+			localStorage.setItem('username', user.username);
+			const username = localStorage.getItem('username');
+			if (username) {
+				isLoginLinkText.innerText = username;
+			}
 		})
 			.catch((err) => {
 				console.error(err);
@@ -45,24 +52,36 @@ const login = () => {
 };
 
 const loginPage = document.querySelector('.js-login');
-if (loginPage) {
-	const token = localStorage.getItem('access_token');
-	const isAuthenticate = () => {
-		const pem = jwkToPem(key);
-		const options = { algorithms: ['RS256'] };
-		try {
-			jwt.verify(token, pem, options);
-			return true;
-		} catch {
-			localStorage.removeItem('access_token');
-			localStorage.removeItem('refresh_token');
-			localStorage.removeItem('expires_in');
-			return false;
+const token = localStorage.getItem('access_token');
+const isAuthenticate = () => {
+	try {
+		jwt.verify(token, pem, options);
+		console.log('true token');
+		const username = localStorage.getItem('username');
+		if (username) {
+			isLoginLinkText.innerText = username;
 		}
-	};
-	if (!token) {
+		return true;
+	} catch {
+		localStorage.removeItem('access_token');
+		localStorage.removeItem('refresh_token');
+		localStorage.removeItem('expires_in');
+		console.log('false token');
+		return false;
+	}
+};
+
+if (!token) {
+	if (loginPage) {
 		login();
-	} else {
-		isAuthenticate();
+	}
+} else {
+	isAuthenticate();
+}
+
+if (loginPage) {
+	const isHiddenLogin = document.querySelector('.menu-desktop__list-item.is-login.is-hidden');
+	if (isHiddenLogin) {
+		isHiddenLogin.classList.remove('is-hidden');
 	}
 }
