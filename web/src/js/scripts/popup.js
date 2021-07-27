eac286ab580db8b9b6ee5090dc6f36f0952520a5
Marcin Owczarzak <marcin@adchitects.co>
2021-07-27 16:23:05 +0200

    Popup tweaks

--- web/src/js/scripts/popup.js
+++ web/src/js/scripts/popup.js
@@ -4,6 +4,7 @@ if (popup) {
 	const closeIcon = popup.querySelector('.js-popup-close-icon');
 	closeBtnAll.forEach(closeBtn => {
 		closeBtn.addEventListener('click', () => {
+			localStorage.setItem('popup', 'shown');
 			popup.classList.remove('is-active');
 			document.body.style.overflow = '';
 		});
@@ -14,8 +15,21 @@ if (popup) {
 			closeIcon.style.color = '';
 		});
 	});
+	document.onkeydown = function(evt) {
+		evt = evt || window.event;
+		let isEscape = false;
+		if ('key' in evt) {
+			isEscape = (evt.key === 'Escape' || evt.key === 'Esc');
+		} else {
+			isEscape = (evt.keyCode === 27);
+		}
+		if (isEscape) {
+			localStorage.setItem('popup', 'shown');
+			popup.classList.remove('is-active');
+			document.body.style.overflow = '';
+		}
+	};
 	if (!localStorage.getItem('popup')) {
-		localStorage.setItem('popup', 'shown');
 		popup.classList.add('is-active');
 		document.body.style.overflow = 'hidden';
 	} else {
