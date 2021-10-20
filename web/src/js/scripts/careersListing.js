f65d913be9967d88ff2782075f55cca2f2782511
Marcin Owczarzak <marcin@adchitects.co>
2021-10-20 15:40:31 +0200

    Coreers Filters & sroting done

--- web/src/js/scripts/careersListing.js
+++ web/src/js/scripts/careersListing.js
@@ -1,6 +1,6 @@
 const careersListing = document.querySelector('.js-careers-listing');
 const careersListingFiltersHld = document.querySelector('.js-careers-listing-filters-hld');
-const careersListingSortsHld = document.querySelector('.js-careers-listing-sort-hld');
+const careersListingSortHld = document.querySelector('.js-careers-listing-sort-hld');
 if (careersListing) {
 	const COMPANY_UID = 'F6.003';
 	const TOKEN = '6F329B2DE6379829B229B2037983E8B3E8B';
@@ -22,6 +22,7 @@ if (careersListing) {
 				careersListing.innerHTML = offersHTML;
 				careersListing.classList.remove('hidden');
 			};
+			offers.sort((a, b) => (a.location.country > b.location.country) ? 1 : (b.location.country > a.location.country) ? -1 : 0);
 			printOffers();
 			// Filtering by city
 			if (careersListingFiltersHld) {
@@ -30,36 +31,62 @@ if (careersListing) {
 				let filtersHTML = '';
 				let cities = [];
 				offers.forEach(offer => {
-					console.log(offer);
 					if (cities.indexOf(offer.location.name) === -1) {
 						cities.push(offer.location.name);
 					}
 				});
-				filtersHTML += `<div class="filter-item js-careers-listing-filter-item-reset is-active" data-city="">All locations</div>`;
+				filtersHTML += `<div class="filter-item js-careers-listing-filter-item-reset is-active" data-city="">All</div>`;
 				cities.forEach(city => {
 					filtersHTML += `<div class="filter-item js-careers-listing-filter-item" data-city="${city.toLowerCase().replace(/\s/g, '')}">${city}</div>`;
 				});
 				filtersDropdown.innerHTML = filtersHTML;
+				let filtersChosenOpened;
 				filtersChosen.addEventListener('click', (e) => {
 					e.stopPropagation();
-					filtersChosen.classList.add('is-active');
-					filtersDropdown.classList.remove('is-hidden');
+					if (!filtersChosenOpened) {
+						filtersChosen.classList.add('is-active');
+						filtersDropdown.classList.remove('is-hidden');
+						filtersChosenOpened = true;
+					} else {
+						filtersChosen.classList.remove('is-active');
+						filtersDropdown.classList.add('is-hidden');
+						filtersChosenOpened = false;
+					}
 				});
 				document.addEventListener('click', () => {
 					filtersChosen.classList.remove('is-active');
 					filtersDropdown.classList.add('is-hidden');
+					filtersChosenOpened = false;
 				});
 				const filterItemAll = careersListingFiltersHld.querySelectorAll('.js-careers-listing-filter-item');
 				const filterItemReset = careersListingFiltersHld.querySelector('.js-careers-listing-filter-item-reset');
 				filterItemAll.forEach(filterItem => {
 					filterItem.addEventListener('click', () => {
+						if (careersListingSortHld) {
+							const sortChosen = careersListingSortHld.querySelector('.js-careers-listing-sort-chosen');
+							const s1 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-1');
+							const s2 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-2');
+							const s3 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-3');
+							const s4 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-4');
+							s1.style.display = 'none';
+							s2.style.display = 'none';
+							if (s1.classList.contains('is-active') || s2.classList.contains('is-active')) {
+								if (s4.classList.contains('is-active')) {
+									sortChosen.innerHTML = 'Sort: ' + s4.innerHTML;
+									s4.classList.add('is-active');
+								} else {
+									sortChosen.innerHTML = 'Sort: ' + s3.innerHTML;
+									s3.classList.add('is-active');
+								}
+							}
+						}
 						const listingItemAll = careersListing.querySelectorAll('.js-careers-listing-item');
 						filterItemAll.forEach(fI => {
 							fI.classList.remove('is-active');
 						});
 						filterItemReset.classList.remove('is-active');
 						filterItem.classList.add('is-active');
-						filtersChosen.innerHTML = filterItem.innerHTML;
+						filtersChosen.innerHTML = 'Location: ' + filterItem.innerHTML;
 						filtersChosen.classList.remove('is-active');
 						filtersDropdown.classList.add('is-hidden');
 						listingItemAll.forEach(listingItem => {
@@ -71,12 +98,22 @@ if (careersListing) {
 					});
 				});
 				filterItemReset.addEventListener('click', () => {
+					if (careersListingSortHld) {
+						const s1 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-1');
+						const s2 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-2');
+						s1.style.display = 'block';
+						s2.style.display = 'block';
+						if (s1.classList.contains('is-active') || s2.classList.contains('is-active')) {
+							s1.classList.remove('is-active');
+							s2.classList.remove('is-active');
+						}
+					}
 					const listingItemAll = careersListing.querySelectorAll('.js-careers-listing-item');
 					filterItemAll.forEach(fI => {
 						fI.classList.remove('is-active');
 					});
 					filterItemReset.classList.add('is-active');
-					filtersChosen.innerHTML = filterItemReset.innerHTML;
+					filtersChosen.innerHTML = 'Location: ' + filterItemReset.innerHTML;
 					listingItemAll.forEach(listingItem => {
 						listingItem.classList.remove('is-hidden');
 					});
@@ -85,50 +122,92 @@ if (careersListing) {
 				});
 			}
 			// Sorting
-			// Country A to Z
-			const filterItemAll = careersListingFiltersHld.querySelectorAll('.js-careers-listing-filter-item');
-			const filterActive = () => {
-				filterItemAll.forEach(filterItem => {
-					if (filterItem.classList.contains('is-active')) {
-						const listingItemAll = careersListing.querySelectorAll('.js-careers-listing-item');
-						listingItemAll.forEach(listingItem => {
-							listingItem.classList.add('is-hidden');
-							if (listingItem.dataset.city === filterItem.dataset.city) {
-								listingItem.classList.remove('is-hidden');
-							}
-						});
+			if (careersListingSortHld) {
+				const sortChosen = careersListingSortHld.querySelector('.js-careers-listing-sort-chosen');
+				const sortDropdown = careersListingSortHld.querySelector('.js-careers-listing-sort-dropdown');
+				let sortChosenOpened;
+				sortChosen.addEventListener('click', (e) => {
+					e.stopPropagation();
+					if (!sortChosenOpened) {
+						sortChosen.classList.add('is-active');
+						sortDropdown.classList.remove('is-hidden');
+						sortChosenOpened = true;
+					} else {
+						sortChosen.classList.remove('is-active');
+						sortDropdown.classList.add('is-hidden');
+						sortChosenOpened = false;
 					}
 				});
-			};
-			const s1 = careersListingSortsHld.querySelector('.js-s1');
-			s1.addEventListener('click', () => {
-				offers.sort((a, b) => (a.location.country > b.location.country) ? 1 : (b.location.country > a.location.country) ? -1 : 0);
-				printOffers();
-			});
-			// Country Z to A
-			const s2 = careersListingSortsHld.querySelector('.js-s2');
-			s2.addEventListener('click', () => {
-				offers.sort((a, b) => (b.location.country > a.location.country) ? 1 : (a.location.country > b.location.country) ? -1 : 0);
-				printOffers();
-			});
-			// Job title A to Z
-			const s3 = careersListingSortsHld.querySelector('.js-s3');
-			s3.addEventListener('click', () => {
-				offers.sort((a, b) => (a.name > b.name) ? 1 : (b.name > a.name) ? -1 : 0);
-				printOffers();
-				if (careersListingFiltersHld) {
-					filterActive();
-				}
-			});
-			// Job title Z to A
-			const s4 = careersListingSortsHld.querySelector('.js-s4');
-			s4.addEventListener('click', () => {
-				offers.sort((a, b) => (b.name > a.name) ? 1 : (a.name > b.name) ? -1 : 0);
-				printOffers();
-				if (careersListingFiltersHld) {
-					filterActive();
-				}
-			});
+				document.addEventListener('click', () => {
+					sortChosen.classList.remove('is-active');
+					sortDropdown.classList.add('is-hidden');
+					sortChosenOpened = false;
+				});
+				const filterItemAll = careersListingFiltersHld.querySelectorAll('.js-careers-listing-filter-item');
+				const filterActive = () => {
+					filterItemAll.forEach(filterItem => {
+						if (filterItem.classList.contains('is-active')) {
+							const listingItemAll = careersListing.querySelectorAll('.js-careers-listing-item');
+							listingItemAll.forEach(listingItem => {
+								listingItem.classList.add('is-hidden');
+								if (listingItem.dataset.city === filterItem.dataset.city) {
+									listingItem.classList.remove('is-hidden');
+								}
+							});
+						}
+					});
+				};
+				// Country A to Z
+				const s1 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-1');
+				s1.addEventListener('click', () => {
+					offers.sort((a, b) => (a.location.country > b.location.country) ? 1 : (b.location.country > a.location.country) ? -1 : 0);
+					printOffers();
+					sortChosen.innerHTML = 'Sort: ' + s1.innerHTML;
+					s2.classList.remove('is-active');
+					s3.classList.remove('is-active');
+					s4.classList.remove('is-active');
+					s1.classList.add('is-active');
+				});
+				// Country Z to A
+				const s2 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-2');
+				s2.addEventListener('click', () => {
+					offers.sort((a, b) => (b.location.country > a.location.country) ? 1 : (a.location.country > b.location.country) ? -1 : 0);
+					printOffers();
+					sortChosen.innerHTML = 'Sort: ' + s2.innerHTML;
+					s1.classList.remove('is-active');
+					s3.classList.remove('is-active');
+					s4.classList.remove('is-active');
+					s2.classList.add('is-active');
+				});
+				// Job title A to Z
+				const s3 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-3');
+				s3.addEventListener('click', () => {
+					offers.sort((a, b) => (a.name > b.name) ? 1 : (b.name > a.name) ? -1 : 0);
+					printOffers();
+					sortChosen.innerHTML = 'Sort: ' + s3.innerHTML;
+					s1.classList.remove('is-active');
+					s2.classList.remove('is-active');
+					s4.classList.remove('is-active');
+					s3.classList.add('is-active');
+					if (careersListingFiltersHld) {
+						filterActive();
+					}
+				});
+				// Job title Z to A
+				const s4 = careersListingSortHld.querySelector('.js-careers-listing-sort-item-4');
+				s4.addEventListener('click', () => {
+					offers.sort((a, b) => (b.name > a.name) ? 1 : (a.name > b.name) ? -1 : 0);
+					printOffers();
+					sortChosen.innerHTML = 'Sort: ' + s4.innerHTML;
+					s1.classList.remove('is-active');
+					s2.classList.remove('is-active');
+					s3.classList.remove('is-active');
+					s4.classList.add('is-active');
+					if (careersListingFiltersHld) {
+						filterActive();
+					}
+				});
+			}
 		})
 		.catch(err => console.error(err));
 }
