3203ec0e1fc8b41181516f936e638951ac609404
Marcin Owczarzak <marcin@adchitects.co>
2021-11-25 15:40:42 +0100

    feat: crs branch init

--- web/src/js/scripts/buyTilesIcons.js
+++ web/src/js/scripts/buyTilesIcons.js
@@ -4,16 +4,18 @@ if (tilesHld) {
 	const tilesAll = tilesHld.querySelectorAll('.js-buy-tiles-item');
 	tilesAll.forEach(item => {
 		const icon = item.querySelector('.js-buy-tiles-icon');
-		const iconAnimation = new Vivus(icon, {
-			start: 'manual',
-			type: 'scenario',
-			animTimingFunction: Vivus.EASE,
-		});
-		iconAnimation.finish();
-		item.addEventListener('mouseenter', () => {
-			iconAnimation.stop();
-			iconAnimation.reset();
-			iconAnimation.play();
-		});
+		if (icon) {
+			const iconAnimation = new Vivus(icon, {
+				start: 'manual',
+				type: 'scenario',
+				animTimingFunction: Vivus.EASE,
+			});
+			iconAnimation.finish();
+			item.addEventListener('mouseenter', () => {
+				iconAnimation.stop();
+				iconAnimation.reset();
+				iconAnimation.play();
+			});
+		}
 	});
 }
