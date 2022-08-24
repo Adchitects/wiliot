628637a31226a4d17f4a29883a588b86fc4c6d13
Marcin Owczarzak <marcin@adchitects.co>
2022-08-24 17:18:01 +0200

    feat: logout in progress

--- web/src/js/scripts/fusionAuth.js
+++ web/src/js/scripts/fusionAuth.js
@@ -9,6 +9,7 @@ const key = {
 	'n': 'pH-2BjhKO4MiPf5VXRVTc_vnBO00CqI0NOq-g3Y5lpIv3a-IYsnswnfhm5yQha7OGlTRh5Nfpys2NZybuzY5MOe5I3EIJEZzHw5racQPsbJ6FmVdiF8O0Pikxgxlq5gihiWjdWQ9dmaqyARwdgbOfpB3jvIFYR7Ca02ykqSvhtEch6sYwJqlZWzmc10v3bVrKLieN6YqDOYdvzG__5VVqpN4Tj-I0aNq5LPfCF0sSiHBS76tl941U0v5Dy1XCZmaINlqehn1MGmZBr9pj40-BX90boly8O67B9rkhYGwicVqUkuFarTZBKA1xEbQuus_KpdgyoIFl5rYM4jGOgKgMw',
 	'use': 'sig',
 };
+const isStaging = window.location.hostname === 'wiliot.adchitects.co';
 const pem = jwkToPem(key);
 const options = { algorithms: ['RS256'] };
 const isLoginLink = document.querySelector('.menu-desktop__list-item.is-login');
@@ -23,8 +24,13 @@ const login = () => {
 	const queryString = window.location.search;
 	const urlParams = new URLSearchParams(queryString);
 	const code = urlParams.get('code');
+	let url;
 	// const url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=${redirectUri}&grant_type=authorization_code`;
-	const url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=https://www.wiliot.com/login&grant_type=authorization_code`;
+	if (!isStaging) {
+		url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=https://www.wiliot.com/login&grant_type=authorization_code`;
+	} else {
+		url = `${host}${path}?code=${code}&client_id=${clientId}&redirect_uri=https://www.wiliot.adchitects.co/web/login&grant_type=authorization_code`;
+	}
 	// console.log(code);
 	if (code) {
 		axios.post(url, null, {}).then((response) => {
@@ -91,17 +97,36 @@ if (!token) {
 const logoutBtn = document.querySelector('.js-logout');
 if (logoutBtn) {
 	logoutBtn.addEventListener('click', () => {
+		console.log('logoutBtn click 44');
+		// const clientId = '21ffd444-76e2-4c3f-9317-38c5fddb169d';
 		localStorage.removeItem('access_token');
 		localStorage.removeItem('refresh_token');
 		localStorage.removeItem('expires_in');
 		localStorage.removeItem('username');
+		window.location = 'https://login.wiliot.com/oauth2/logout?client_id=21ffd444-76e2-4c3f-9317-38c5fddb169d&tenantId=38623562-3435-3632-6538-653563353564';
+
+		// const xmlhttp = new XMLHttpRequest();
+		// xmlhttp.open('POST', 'https://login.wiliot.com/oauth2/logout?client_id=21ffd444-76e2-4c3f-9317-38c5fddb169d&tenantId=38623562-3435-3632-6538-653563353564', true);
+		// xmlhttp.onload = () => {
+		// 	console.log('Request finished. Do processing here.');
+		// 	setTimeout(() => {
+		// 		window.location = 'https://wiliot.com/';
+		// 	}, 1000);
+		// };
+		// xmlhttp.send(null);
+
+
+		// setTimeout(() => {
+		// 	window.location = 'https://wiliot.com/';
+		// }, 1000);
 	});
 }
 
-const logoutPage = document.querySelector('#js-logout-page');
-if (logoutPage) {
-	localStorage.removeItem('access_token');
-	localStorage.removeItem('refresh_token');
-	localStorage.removeItem('expires_in');
-	localStorage.removeItem('username');
-}
+// const logoutPage = document.querySelector('#js-logout-page');
+// if (logoutPage) {
+// 	localStorage.removeItem('access_token');
+// 	localStorage.removeItem('refresh_token');
+// 	localStorage.removeItem('expires_in');
+//     localStorage.removeItem('username');
+//     window.location = 'https://login.wiliot.com/oauth2/logout?client_id=21ffd444-76e2-4c3f-9317-38c5fddb169d&tenantId=38623562-3435-3632-6538-653563353564'
+// }
