e91f6976d85a93b22765d13cd403fdbf4d083963
Wojciech Kalbarczyk <wojciech@adchitects.co>
2022-12-22 15:21:57 +0100

    feat: podcast listing rework

--- web/src/js/scripts/video.js
+++ web/src/js/scripts/video.js
@@ -1,30 +1,34 @@
 const videoHldAll = document.querySelectorAll('.js-video-hld');
-videoHldAll.forEach(hld => {
-	const vid = hld.querySelector('video');
-	let videoIsPlayed = hld.classList.contains('is-paused') ? false : true;
-	vid.addEventListener('click', () => {
-		hld.classList.remove('show-thumbnail');
-		hld.classList.remove('is-before-play');
-		if (!videoIsPlayed) {
-			vid.play();
-			videoIsPlayed = true;
-			hld.classList.remove('is-paused');
-			setTimeout(() => {
-				vid.setAttribute('controls', 'controls');
-			}, 500);
-		} else {
-			if (vid.hasAttribute('controls')) {
-				vid.removeAttribute('controls');
-				setTimeout(() => {
-					vid.pause();
-					videoIsPlayed = false;
-					hld.classList.add('is-paused');
-				}, 500);
-			} else {
-				vid.pause();
-				videoIsPlayed = false;
-				hld.classList.add('is-paused');
-			}
+if (videoHldAll) {
+	videoHldAll.forEach(hld => {
+		const vid = hld.querySelector('video');
+		let videoIsPlayed = hld.classList.contains('is-paused') ? false : true;
+		if (vid) {
+			vid.addEventListener('click', () => {
+				hld.classList.remove('show-thumbnail');
+				hld.classList.remove('is-before-play');
+				if (!videoIsPlayed) {
+					vid.play();
+					videoIsPlayed = true;
+					hld.classList.remove('is-paused');
+					setTimeout(() => {
+						vid.setAttribute('controls', 'controls');
+					}, 500);
+				} else {
+					if (vid.hasAttribute('controls')) {
+						vid.removeAttribute('controls');
+						setTimeout(() => {
+							vid.pause();
+							videoIsPlayed = false;
+							hld.classList.add('is-paused');
+						}, 500);
+					} else {
+						vid.pause();
+						videoIsPlayed = false;
+						hld.classList.add('is-paused');
+					}
+				}
+			});
 		}
 	});
-});
+}
