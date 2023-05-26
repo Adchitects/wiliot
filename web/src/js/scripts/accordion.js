e980438cb16496a277f96db4dd4f087db4e88174
Ad_Workstation_42 <ad_workstation_42@Ad-Workstation-42s-MacBook-Pro.local>
2023-05-26 13:19:23 +0200

    fix: console logs clear

--- web/src/js/scripts/accordion.js
+++ web/src/js/scripts/accordion.js
@@ -1,12 +1,9 @@
 import { clearClassActive } from './utilities';
 const accordionHldAll = document.querySelectorAll('.js-accordion');
 accordionHldAll.forEach(accordion => {
-	console.log('accordion');
 	const accordionItemAll = accordion.querySelectorAll('.js-accordion-toggle-item');
-	console.log(accordion.querySelector('.js-accordion-toggle-item'));
 	accordionItemAll.forEach(item => {
 		item.addEventListener('click', () => {
-			console.log('click');
 			if (item.classList.contains('is-active')) {
 				item.classList.remove('is-active');
 			} else {
