4fa1a5a3989e337241efdb446247a7e1261f954b
Ad_Workstation_42 <ad_workstation_42@Ad-Workstation-42s-MacBook-Pro.local>
2023-07-27 15:17:25 +0200

    perf: lottie lazyload + hero-slider video

--- web/src/js/scripts/homepageLottieAnimation.js
+++ web/src/js/scripts/homepageLottieAnimation.js
@@ -2,32 +2,38 @@ import Lottie from 'lottie-web';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 gsap.registerPlugin(ScrollTrigger);
-const animationHld = document.querySelector('.js-homepage-animation-hld');
-if (animationHld) {
-	const animationAll = animationHld.querySelectorAll('.js-homepage-animation');
-	animationAll.forEach(animation => {
-		const anim = Lottie.loadAnimation({
-			container: animation,
-			renderer: 'svg',
-			loop: true,
-			autoplay: true,
-			path: animation.dataset.animationPath,
-		});
-		gsap.to(animationHld, {
-			scrollTrigger: {
-				// markers: true,
-				trigger: animationHld,
-				start: 'top 95%',
-				end: 'bottom 10%',
-				scrub: true,
-				onEnter: () => anim.play(),
-				onEnterBack: () => anim.play(),
-				onLeave: () => anim.pause(),
-				onLeaveBack: () => anim.pause(),
+
+const animationAll = document.querySelectorAll('.js-homepage-animation');
+animationAll.forEach(animation => {
+	gsap.to({}, {
+		scrollTrigger: {
+			trigger: animation,
+			start: 'top 130%',
+			once: true,
+			onEnter: () => {
+				const anim = Lottie.loadAnimation({
+					container: animation,
+					renderer: 'svg',
+					loop: true,
+					autoplay: true,
+					path: animation.dataset.animationPath,
+				});
+				gsap.to(animation, {
+					scrollTrigger: {
+						trigger: animation,
+						start: 'top 95%',
+						end: 'bottom 10%',
+						scrub: true,
+						onEnter: () => anim.play(),
+						onEnterBack: () => anim.play(),
+						onLeave: () => anim.pause(),
+						onLeaveBack: () => anim.pause(),
+					},
+				});
 			},
-		});
+		},
 	});
-}
+});
 
 window.addEventListener('load', () => {
 	ScrollTrigger.refresh();
