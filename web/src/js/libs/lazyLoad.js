7c7ba734247be6a3029f64bd420d7fc10ccede2a
Marcin Owczarzak <mar.owczarzak@gmail.com>
2023-11-23 14:22:13 +0100

    refactor: lazyload config

--- web/src/js/libs/lazyLoad.js
+++ web/src/js/libs/lazyLoad.js
@@ -6,7 +6,7 @@ gsap.registerPlugin(ScrollTrigger);
 export const lazyLoadInstance = new LazyLoad({
 	elements_selector: '.js-lazy',
 	callback_loaded: ScrollTrigger.refresh(),
-	threshold: 250,
+	// threshold: 250,
 	restore_on_error: true,
 	// use_native: false,
 	// container: document.querySelector('.js-your-smooth-scroll-container'),
