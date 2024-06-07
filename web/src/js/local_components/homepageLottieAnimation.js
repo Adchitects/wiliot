5df560f361fe23ba8e29058ae180063872357d53
Marcin Owczarzak <mar.owczarzak@gmail.com>
2024-06-07 02:00:00 +0200

    sync: backup

--- web/src/js/local_components/homepageLottieAnimation.js
+++ web/src/js/local_components/homepageLottieAnimation.js
@@ -1,42 +1,55 @@
-console.log('homepageLottieAnimation script lazy-loaded');
+// console.log('homepageLottieAnimation script lazy-loaded');
 
-import Lottie from 'lottie-web';
-import gsap from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
-gsap.registerPlugin(ScrollTrigger);
+// import Lottie from 'lottie-web';
+// import gsap from 'gsap';
+// import { ScrollTrigger } from 'gsap/ScrollTrigger';
+// gsap.registerPlugin(ScrollTrigger);
 
-const animationAll = document.querySelectorAll('.js-homepage-animation');
-animationAll.forEach(animation => {
-	gsap.to({}, {
-		scrollTrigger: {
-			trigger: animation,
-			start: 'top 150%',
-			once: true,
-			onEnter: () => {
-				const anim = Lottie.loadAnimation({
-					container: animation,
-					renderer: 'svg',
-					loop: true,
-					autoplay: true,
-					path: animation.dataset.animationPath,
-				});
-				gsap.to(animation, {
-					scrollTrigger: {
-						trigger: animation,
-						start: 'top 95%',
-						end: 'bottom 10%',
-						scrub: true,
-						onEnter: () => anim.play(),
-						onEnterBack: () => anim.play(),
-						onLeave: () => anim.pause(),
-						onLeaveBack: () => anim.pause(),
-					},
-				});
-			},
-		},
-	});
-});
+// const setupHomepageAnimation = (animation) => {
+// 	const rwd = animation.dataset.rwd;
+// 	const mediaQuery = rwd === 'mobile' ? '(max-width: 767px)' : rwd === 'desktop' ? '(min-width: 768px)' : '';
+// 	if (mediaQuery && window.matchMedia(mediaQuery).matches) {
+// 		const anim = Lottie.loadAnimation({
+// 			container: animation,
+// 			renderer: 'svg',
+// 			loop: true,
+// 			autoplay: false,
+// 			path: animation.dataset.animationPath,
+// 		});
+// 		gsap.to(animation, {
+// 			scrollTrigger: {
+// 				trigger: animation,
+// 				start: 'top 95%',
+// 				end: 'bottom 10%',
+// 				scrub: true,
+// 				// markers: true,
+// 				onEnter: () => anim.play(),
+// 				onEnterBack: () => anim.play(),
+// 				onLeave: () => anim.pause(),
+// 				onLeaveBack: () => anim.pause(),
+// 			},
+// 		});
+// 		return anim;
+// 	}
+// 	return null;
+// };
 
-window.addEventListener('load', () => {
-	ScrollTrigger.refresh();
-});
+// const initializeHomepageAnimations = () => {
+// 	ScrollTrigger.refresh();
+// 	const homepageAnimationsAll = document.querySelectorAll('.js-homepage-animation');
+// 	let homepageAnimations = Array.from(homepageAnimationsAll).map(setupHomepageAnimation);
+
+// 	const handleResizeHomepage = () => {
+// 		homepageAnimations.forEach((anim, index) => {
+// 			if (anim) {
+// 				anim.destroy();
+// 				homepageAnimations[index] = null;
+// 			}
+// 		});
+// 		homepageAnimations = Array.from(homepageAnimationsAll).map(setupHomepageAnimation);
+// 	};
+
+// 	window.addEventListener('resize', handleResizeHomepage);
+// };
+
+// initializeHomepageAnimations();
---
+++ web/src/js/local_components/supplyChainLottieAnimation.js
@@ -0,0 +1,54 @@
+console.log('supplyChainLottieAnimation script lazy-loaded');
+
+import Lottie from 'lottie-web';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
+
+const setupSupplyChainAnimation = (animation) => {
+	const rwd = animation.dataset.rwd;
+	const mediaQuery = rwd === 'mobile' ? '(max-width: 767px)' : rwd === 'desktop' ? '(min-width: 768px)' : '';
+	if (mediaQuery && window.matchMedia(mediaQuery).matches) {
+		const anim = Lottie.loadAnimation({
+			container: animation,
+			renderer: 'svg',
+			loop: true,
+			autoplay: false,
+			path: animation.dataset.animationPath,
+		});
+		gsap.to(animation, {
+			scrollTrigger: {
+				trigger: animation,
+				start: 'top 95%',
+				end: 'bottom 10%',
+				scrub: true,
+				onEnter: () => anim.play(),
+				onEnterBack: () => anim.play(),
+				onLeave: () => anim.pause(),
+				onLeaveBack: () => anim.pause(),
+			},
+		});
+		return anim;
+	}
+	return null;
+};
+
+const initializeSupplyChainAnimations = () => {
+	ScrollTrigger.refresh();
+	const supplyChainAnimationsAll = document.querySelectorAll('.js-lottie-animation');
+	let supplyChainAnimations = Array.from(supplyChainAnimationsAll).map(setupSupplyChainAnimation);
+
+	const handleResizeSupplyChain = () => {
+		supplyChainAnimations.forEach((anim, index) => {
+			if (anim) {
+				anim.destroy();
+				supplyChainAnimations[index] = null;
+			}
+		});
+		supplyChainAnimations = Array.from(supplyChainAnimationsAll).map(setupSupplyChainAnimation);
+	};
+
+	window.addEventListener('resize', handleResizeSupplyChain);
+};
+
+initializeSupplyChainAnimations();
