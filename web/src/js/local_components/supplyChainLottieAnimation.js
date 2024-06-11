37833e2f049b9da39fd17e643dc2e17b59efac9b
Marcin Owczarzak <mar.owczarzak@gmail.com>
2024-06-11 02:06:56 +0200

    feat: hover areas

--- web/src/js/local_components/supplyChainLottieAnimation.js
+++ web/src/js/local_components/supplyChainLottieAnimation.js
@@ -1,23 +1,20 @@
 setTimeout(() => {
-	const warehouseTargetElements = document.querySelectorAll('g[transform="matrix(1,0,0,1,380.34698486328125,251.5240020751953)"], g[transform="matrix(0.3333300054073334,0,0,0.3333300054073334,140.50054931640625,252.0009307861328)"]');
-	const transportTargetElements = document.querySelectorAll('g[transform="matrix(1,0,0,1,686.802001953125,251.5240020751953)"], g[transform="matrix(-0.3499999940395355,0,0,0.3499999940395355,554.9000244140625,338.20001220703125)"]');
-	const warehouseAnimation = document.querySelector('.js-supply-chain-lottie-animation[data-label="warehouse"]');
-	const transportAnimation = document.querySelector('.js-supply-chain-lottie-animation[data-label="transport"]');
-	console.log(warehouseTargetElements, transportTargetElements);
+	const hldAll = document.querySelectorAll('.js-supply-chain-lottie-animation-hld');
+	hldAll.forEach(hld => {
+		const labelHoverAreaAll = hld.querySelectorAll('.js-supply-chain-lottie-animation-hover-area');
+		const labelAll = hld.querySelectorAll('.js-supply-chain-lottie-animation-label');
 
-	const addHoverEffects = (targetElements, animation) => {
-		if (targetElements.length > 0 && animation) {
-			targetElements.forEach(targetElement => {
-				targetElement.addEventListener('mouseover', function() {
-					animation.classList.add('is-active');
-				});
-				targetElement.addEventListener('mouseout', function() {
-					animation.classList.remove('is-active');
-				});
+		labelHoverAreaAll.forEach(area => {
+			labelAll.forEach(label => {
+				if (area && label && area.dataset.label === label.dataset.label) {
+					area.addEventListener('mouseover', function() {
+						label.classList.add('is-active');
+					});
+					area.addEventListener('mouseout', function() {
+						label.classList.remove('is-active');
+					});
+				}
 			});
-		}
-	};
-
-	addHoverEffects(warehouseTargetElements, warehouseAnimation);
-	addHoverEffects(transportTargetElements, transportAnimation);
+		});
+	});
 }, 2000);
