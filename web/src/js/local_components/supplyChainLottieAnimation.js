8f165ec7156f67bcb4ad5ed0fc3dc229f952c428
Marcin Owczarzak <mar.owczarzak@gmail.com>
2024-09-04 03:04:36 +0200

    feat: animation hover state

--- web/src/js/local_components/supplyChainLottieAnimation.js
+++ web/src/js/local_components/supplyChainLottieAnimation.js
@@ -1,52 +1,37 @@
 console.log('supplyChainLottieAnimation script lazy-loaded');
-import { gsap } from 'gsap';
 
 function setupHoverEffects() {
 	const hldAll = document.querySelectorAll('.js-supply-chain-lottie-animation-hld');
 	hldAll.forEach(hld => {
 		const labelHoverAreaAll = hld.querySelectorAll('.js-supply-chain-lottie-animation-hover-area');
 		const labelAll = hld.querySelectorAll('.js-supply-chain-lottie-animation-label');
-
+		const svgBoxMap = {
+			'warehouse': '.js-warehouse-box-hoverable',
+			'transport': '.js-transport-box-hoverable',
+			'distribution': '.js-distribution-box-hoverable',
+			'return': '.js-return-logistics-box-hoverable',
+			'home': '.js-home-box-hoverable',
+			'store': '.js-store-box-hoverable',
+			'delivery': '.js-delivery-box-hoverable',
+			'farm': '.js-farm-box-hoverable',
+			'processing': '.js-processing-box-hoverable',
+		};
 		labelHoverAreaAll.forEach(area => {
 			labelAll.forEach(label => {
 				if (area && label && area.dataset.label === label.dataset.label) {
-					area.addEventListener('mouseover', function() {
-						label.classList.add('is-active');
-					});
-					area.addEventListener('mouseout', function() {
-						label.classList.remove('is-active');
+					['mouseenter', 'mouseleave'].forEach(eventType => {
+						area.addEventListener(eventType, function() {
+							const isEnter = eventType === 'mouseenter';
+							label.classList.toggle('is-active', isEnter);
+							if (area.classList.contains('is-link')) {
+								const svgBoxSelector = svgBoxMap[area.dataset.label];
+								const svgBox = hld.querySelector(svgBoxSelector);
+								svgBox?.classList.toggle('is-active', isEnter);
+							}
+						});
 					});
 				}
 			});
-
-			// Add floating text for elements with .is-link class
-			if (area.classList.contains('is-link')) {
-				const boxText = document.createElement('div');
-				boxText.classList.add('js-supply-chain-lottie-animation-text');
-				boxText.textContent = 'Click to learn more';
-				boxText.style.position = 'absolute';
-				boxText.style.bottom = '-14px';
-				boxText.style.right = '-50px';
-				boxText.style.pointerEvents = 'none';
-				boxText.style.opacity = '0';
-				boxText.style.zIndex = '99992';
-				boxText.style.color = '#27BCBC';
-				boxText.style.padding = '4px 10px 2px';
-				boxText.style.fontSize = '16px';
-				boxText.style.fontWeight = '500';
-				boxText.style.whiteSpace = 'nowrap';
-				boxText.style.borderRadius = '4px';
-				area.appendChild(boxText);
-
-
-				area.addEventListener('mouseenter', () => {
-					gsap.to(boxText, { opacity: 1, duration: 0.3 });
-				});
-
-				area.addEventListener('mouseleave', () => {
-					gsap.to(boxText, { opacity: 0, duration: 0.3 });
-				});
-			}
 		});
 	});
 }
